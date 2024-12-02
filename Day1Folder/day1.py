e5d0279bf9f50743dde0f6d7bfc24518ec56ad1d
natalie162 <nataliechee689@gmail.com>
2024-12-02 04:29:30 +0000

    Day 1
    Co-authored-by: Computhink-Alfred <Computhink-Alfred@users.noreply.github.com>

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,4 +1,57 @@
 # Write all your codes for Day 1 here.
 # REMEMBER to change main.py to import this file.
 # You can COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1")
+# print("My name is natalie")
+# print("I am 13 years old")
+# print("I attend Paya Lebar Methodist Girls' School")
+# print("If I had $2,000 dollars, I would buy a new phone")
+
+# animal1 = "Monkey"
+# animal2 = "Elephant"
+# animal3 = "Lion"
+
+# print(animal1)
+# print(animal2)
+# print(animal3)
+
+# variable1 = 3
+# variable2 = 5
+
+# print (variable1 + variable2)
+
+# name = "Dog "
+# food = "bone" 
+
+# print("I gave my " + name + "a " + food) 
+
+# def intro():
+#     print("My name is Natalie")
+#     print("I am 13 this year")
+#     print("I am currently learning Python")
+
+# intro()
+
+# def intro(name):
+#     print("my name is " + name)
+#     print("i am currntly 13 this year")
+#     print("i am currently learning python")
+
+# intro("John") 
+# intro("James")
+# intro("Johnathan")  
+
+# def intro(name, age):
+#     print("My name is " + name)
+#     print("I am " + age + " this year")
+#     print("I am currently learning Python")
+
+# intro("John", "20")
+# intro("James", "25")   
+
+def sumOf(num1, num2):
+    print(num1 + num2)
+
+sumOf(5, 10) 
+sumOf(20, 30) 
+sumOf(40, 5)
